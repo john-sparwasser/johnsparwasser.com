e023f014d0090c8ac0068f51382d8826f5f57894
John Sparwasser <john.sparwasser@foundry.ai>
2024-09-20 17:19:46 -0400

    Fixed a typo

--- gulpfile.js
+++ gulpfile.js
@@ -5,19 +5,19 @@ var vueify = require("vueify");
 var sass = require("gulp-sass");
 
 gulp.task('vue', function() {
-    return browserify("./assets/vue/app.js")
-        .transform(vueify)
-        .bundle()
-        .pipe(fs.createWriteStream("./assets/compiled/js/script.js"))
+  return browserify("./assets/vue/app.js")
+    .transform(vueify)
+    .bundle()
+    .pipe(fs.createWriteStream("./assets/compiled/js/script.js"))
 });
 
 gulp.task('sass', function() {
-    return gulp.src('./assets/sass/includes.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./assets/compiled/css'));
+  return gulp.src('./assets/sass/includes.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./assets/compiled/css'));
 });
 
 gulp.task('default', ['vue', 'sass'], function() {
-    gulp.watch('./assets/vue/*.js', ['vue']);
-    gulp.watch('./assets/sass/*.scss', ['sass']);
+  gulp.watch('./assets/vue/*.js', ['vue']);
+  gulp.watch('./assets/sass/*.scss', ['sass']);
 });
